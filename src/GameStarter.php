0efd810a48bf1a1cac488489930ef8d1540170cc
VITALIY <integralik@gmail.com>
2020-07-24 15:50:21 +0300

    Added Brain-prime game

--- src/GameStarter.php
+++ src/GameStarter.php
@@ -5,6 +5,7 @@ namespace Brain\Games;
 use Brain\Games\Games\BrainCalc;
 use Brain\Games\Games\BrainEven;
 use Brain\Games\Games\BrainGreatCommonDivisor;
+use Brain\Games\Games\BrainPrime;
 use Brain\Games\Games\BrainProgression;
 
 class GameStarter
@@ -12,14 +13,17 @@ class GameStarter
     public static function getGame($gameTitle)
     {
         switch ($gameTitle) {
-            case 'brain-even':
-                $game = new BrainEven();
-                break;
             case 'brain-calc':
                 $game = new BrainCalc();
                 break;
+            case 'brain-even':
+                $game = new BrainEven();
+                break;
             case 'brain-gcd':
                 $game = new BrainGreatCommonDivisor();
+                break;
+            case 'brain-prime':
+                $game = new BrainPrime();
                 break;
             case 'brain-progression':
                 $game = new BrainProgression();
---
+++ src/Games/BrainPrime.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Brain\Games\Games;
+
+use Brain\Games\AGame;
+
+class BrainPrime extends AGame
+{
+    public const MAX_NUMBER = 100;
+
+
+    public $lastCorrectAnswer = null;
+
+    public function __construct()
+    {
+    }
+
+    public function getWelcomeMessage()
+    {
+        return 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    }
+
+    public function getQuestionInfo()
+    {
+        $number = rand(0, self::MAX_NUMBER);
+
+        $text = $number;
+        $info = $number;
+
+        return [$text, $info];
+    }
+
+    public function getCorrectAnswer($questionData)
+    {
+        $number = $questionData;
+
+        $start_divisor = 2;
+
+        $answer = "yes";
+        for ($i = $number - 1; $i >= $start_divisor; $i--) {
+            if ($number % $i == 0) {
+                $answer = 'no';
+                break;
+            }
+        }
+
+        $this->lastCorrectAnswer = $answer;
+        return $answer;
+    }
+}
